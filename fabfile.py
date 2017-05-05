8c19815731922ad23911d9eb0aec93982f26590d
KarunakarDevOps <karunakarg1990@gmail.com>
2017-05-05 16:44:56 +0530

    first

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,7 @@
 from fabric.api import local
 
-
 def myfunct():
-	local("cd /home/karunakar/python/scripts/github-devops && mkdir devops dev1 dev2")
-	local("cd /home/karunakar/python/scripts/github-devops && git add . && git commit -m 'firstcommit'")
-	local("cd /home/karunakar/python/scripts/github-devops && git push origin  master")
+	local("cd /home/karunakar/python/scripts/github-devops && touch devops dev1 dev2")
+	local("cd /home/karunakar/python/scripts/github-devops && git add . && git commit -m 'first'")
+	local("cd /home/karunakar/python/scripts/github-devops && git push origin master")
   
